c182e6acef7eb109706c8d965115d63053a75379
Dane-lv <danils.cumikovs@gmail.com>
2025-03-29 13:25:29 +0100

    player movement ver1

--- source/player.c
+++ source/player.c
@@ -5,13 +5,12 @@
 
 struct player
 {
-    float x;
-    float y;
-    float playerSpeed;
+    float x, y; //player movement math
+    float vy, vx;
     int angle;
     SDL_Renderer *pRenderer;
     SDL_Texture *pTexture;
-    SDL_Rect playerRect;
+    SDL_Rect playerRect; //rect.x rect.y for rendering after movement math
 }; 
 
 Player *createPlayer(int x, int y, SDL_Renderer *pRenderer)
@@ -19,7 +18,8 @@ Player *createPlayer(int x, int y, SDL_Renderer *pRenderer)
     Player *pPlayer = malloc(sizeof(struct player));
     pPlayer->x = x;
     pPlayer->y = y;
-    pPlayer->playerSpeed = PLAYERSPEED;
+    pPlayer->vy = pPlayer->vx = 0;
+    pPlayer->angle = 0;
     SDL_Surface *pSurface = IMG_Load("resources/soldiertopdown.png");
     if(!pSurface){
         printf("Error: %s\n",SDL_GetError());
@@ -42,4 +42,48 @@ Player *createPlayer(int x, int y, SDL_Renderer *pRenderer)
 void drawPlayer(Player *pPlayer)
 {
     SDL_RenderCopy(pPlayer->pRenderer,pPlayer->pTexture,NULL,&(pPlayer->playerRect));
+}
+
+void updatePlayer(Player *pPlayer, float deltaTime)
+{
+    pPlayer->x += pPlayer->vx * deltaTime;
+    pPlayer->y += pPlayer->vy * deltaTime;
+    pPlayer->playerRect.x = (int)pPlayer->x;
+    pPlayer->playerRect.y = (int)pPlayer->y;
+} 
+
+void movePlayerLeft(Player *pPlayer) {
+    pPlayer->vx = -PLAYERSPEED;
+    pPlayer->vy = 0;
+}
+
+void movePlayerRight(Player *pPlayer) {
+    pPlayer->vx = +PLAYERSPEED;
+    pPlayer->vy = 0;
+}
+
+void movePlayerUp(Player *pPlayer) {
+    pPlayer->vy = -PLAYERSPEED;
+    pPlayer->vx = 0; 
+}
+
+void movePlayerDown(Player *pPlayer) {
+    pPlayer->vy = +PLAYERSPEED;
+    pPlayer->vx = 0;
+} 
+
+void stopMovementVY(Player *pPlayer)
+{
+    pPlayer->vy = 0;   
+}
+
+void stopMovementVX(Player *pPlayer)
+{
+    pPlayer->vx = 0;   
+}
+
+
+void destroyPlayer(Player *pPlayer)
+{
+    free(pPlayer);
 }
