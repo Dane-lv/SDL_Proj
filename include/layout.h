826b458c4917962fb0850084dbf044af5dac414b
Edis Avdic <edis_123@live.se>
2025-04-02 14:21:25 +0200

    Initiated functions from layout.c

---
+++ include/layout.h
@@ -0,0 +1,12 @@
+#ifndef LAYOUT_H
+#define LAYOUT_H
+
+#include <SDL.h>
+
+SDL_Texture* initiateMap(SDL_Renderer *pRenderer);
+SDL_Texture* initiateMaze(SDL_Renderer *pRenderer);
+void drawMap(SDL_Renderer *pRenderer, SDL_Texture *bgTexture);
+void createMaze(SDL_Renderer *pRenderer, SDL_Texture *wallTexture);
+void destroyMap(SDL_Texture *bgTexture);
+
+#endif
